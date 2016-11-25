5a945ed8beaf26c364ab55cefb714f2305855367
Nikhil Prashar <nnprasha@syr-208-035.syr.edu>
2016-11-24 22:33:51 -0500

    Initial Commit

---
+++ ShuttleServiceApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShuttleServiceApp
+//
+//  Created by Nikhil Prashar on 11/24/16.
+//  Copyright © 2016 Nikhil Prashar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
